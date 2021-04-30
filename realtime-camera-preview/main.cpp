21a50cc31b4a8661be63a9be9266abd618685508
Ihar Tumašyk <ihar.tumashyk@banuba.com>
2021-04-30 09:56:12 +0300

    Repair GL render

--- realtime-camera-preview/main.cpp
+++ realtime-camera-preview/main.cpp
@@ -6,9 +6,9 @@ int main()
 {
     BanubaSdkManager sdk(
         "Realtime Camera Preview",
-        {BanubaSdkManager::sdk_resources_path()},
+        {BanubaSdkManager::sdk_resources_path(), BNB_RESOURCES_FOLDER},
         BNB_CLIENT_TOKEN);
-    sdk.load_effect("effects/Afro", true);
+    sdk.load_effect("effects/TrollGrandma", true);
     sdk.process_camera();
     sdk.run_main_loop();
     return 0;
--- single-image-processing/main.cpp
+++ single-image-processing/main.cpp
@@ -5,7 +5,7 @@
 int main()
 {
     BanubaSdkManager sdk("Single Image Processing", {BNB_RESOURCES_FOLDER}, BNB_CLIENT_TOKEN);
-    sdk.load_effect("effects/Afro", true);
+    sdk.load_effect("effects/TrollGrandma", true);
     auto img = path(BNB_RESOURCES_FOLDER) / "face720x1280.jpg";
     sdk.process_image(img);
     return 0;
