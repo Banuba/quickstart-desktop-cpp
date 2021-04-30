2c5b670daacc57016980bc7660f88bb85be11e86
Ihar Tumašyk <ihar.tumashyk@banuba.com>
2021-04-30 14:41:04 +0300

    Repair single photo processing

--- single-image-processing/main.cpp
+++ single-image-processing/main.cpp
@@ -2,11 +2,22 @@
 
 #include <BanubaSdkManager.hpp>
 
+using namespace bnb::interfaces;
+
 int main()
 {
-    BanubaSdkManager sdk("Single Image Processing", {BNB_RESOURCES_FOLDER}, BNB_CLIENT_TOKEN);
+    #if BNB_GL_BACKEND
+    effect_player::set_render_backend(render_backend_type::opengl);
+    #else
+    effect_player::set_render_backend(render_backend_type::wgpu);
+    #endif
+    
+    BanubaSdkManager sdk(
+        "Single Image Processing",
+        {BanubaSdkManager::sdk_resources_path(), BNB_RESOURCES_FOLDER},
+        BNB_CLIENT_TOKEN);
     sdk.load_effect("effects/TrollGrandma", true);
     auto img = path(BNB_RESOURCES_FOLDER) / "face720x1280.jpg";
-    sdk.process_image(img);
+    sdk.process_image(img); // Will right result near executable or in app bundle Resources
     return 0;
 }
