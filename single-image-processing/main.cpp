fb2d973437e7064307225bae9e7ce1deab1a971b
Ihar Tumašyk <ihar.tumashyk@banuba.com>
2021-05-03 11:52:49 +0300

    Repair single photo processing

--- single-image-processing/main.cpp
+++ single-image-processing/main.cpp
@@ -4,9 +4,12 @@
 
 int main()
 {
-    BanubaSdkManager sdk("Single Image Processing", {BNB_RESOURCES_FOLDER}, BNB_CLIENT_TOKEN);
+    BanubaSdkManager sdk(
+        "Single Image Processing",
+        {BanubaSdkManager::sdk_resources_path(), BNB_RESOURCES_FOLDER},
+        BNB_CLIENT_TOKEN);
     sdk.load_effect("effects/Afro", true);
     auto img = path(BNB_RESOURCES_FOLDER) / "face720x1280.jpg";
-    sdk.process_image(img);
+    sdk.process_image(img); // Will right result near executable or in app bundle Resources
     return 0;
 }
