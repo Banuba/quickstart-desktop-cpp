8a66f19171db02b64ac3edbf27f2e4dff3d8ca65
Ihar Tumašyk <ihar.tumashyk@banuba.com>
2021-05-03 11:48:42 +0300

    Repair build for OSX

---
+++ bnb_sdk_manager/src/SdkResourcesPath.cpp
@@ -0,0 +1,5 @@
+#include "BanubaSdkManager.hpp"
+
+std::string BanubaSdkManaher::sdk_resources_path() {
+    return BNB_RESOURCES_FOLDER;
+}
--- realtime-camera-preview/main.cpp
+++ realtime-camera-preview/main.cpp
@@ -4,7 +4,10 @@
 
 int main()
 {
-    BanubaSdkManager sdk("Realtime Camera Preview", {BNB_RESOURCES_FOLDER}, BNB_CLIENT_TOKEN);
+    BanubaSdkManager sdk(
+        "Realtime Camera Preview",
+        {BanubaSdkManager::sdk_resources_path()},
+        BNB_CLIENT_TOKEN);
     sdk.load_effect("effects/Afro", true);
     sdk.process_camera();
     sdk.run_main_loop();
